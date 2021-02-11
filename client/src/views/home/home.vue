ce84d9f7c588bd9ef205b562461c32c622d49e84
Jared <jarednkrause@gmail.com>
2021-02-11 11:46:51 -0500

    remove old files

--- client/src/views/home/home.vue
+++ client/src/views/home/home.vue
@@ -2,32 +2,15 @@
   <div>
     <current-weather></current-weather>
     <days-grid></days-grid>
-    <!--div class="container-fluid">
-      <div class="row">
-        <div class="col">
-          <br><br>
-          <chunked-weather></chunked-weather>
-          <br><br>
-          <hourly-weather></hourly-weather>
-        </div>
-      </div>
-    </div-->
   </div>
 </template>
 
 <script>
-// import currentWeather from '@/components/currentWeather/currentWeather.vue';
-// import chunkedWeather from '@/components/chunkedWeather/chunkedWeather.vue';
-
 import daysGrid from '@/components/days-grid/days-grid.vue';
-// import hourlyWeather from '@/components/hourlyWeather/hourlyWeather.vue';
 
 export default {
   components: {
     'days-grid': daysGrid,
-    // 'current-weather': currentWeather,
-    // 'chunked-weather': chunkedWeather,
-    // 'hourly-weather': hourlyWeather,
   },
 };
 </script>
