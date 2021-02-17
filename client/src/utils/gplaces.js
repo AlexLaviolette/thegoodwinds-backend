2f000f277053cf8da2508b64f462429b59e39cc5
Alex Laviolette <alex.laviolette92@gmail.com>
2021-02-16 19:45:57 -0500

    Working gmaps with search box

--- client/src/utils/gplaces.js
+++ client/src/utils/gplaces.js
@@ -33,9 +33,10 @@ export default function init() {
   const script = document.createElement('script');
   script.async = true;
   script.defer = true;
-  script.src = `https://maps.googleapis.com/maps/api/js?key=${API_KEY}&callback=${CALLBACK_NAME}`;
+  script.src = `https://maps.googleapis.com/maps/api/js?key=${API_KEY}&callback=${CALLBACK_NAME}&libraries=places&v=weekly`;
   script.onerror = rejectInitPromise;
   document.querySelector('head').appendChild(script);
 
   return initPromise;
 }
+
